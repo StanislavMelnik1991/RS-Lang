b78aa16726d8ae36cd56b57e66d689e0028ee4a5
Stanislav Melnik <stanislavmelnik@gmail.com>
2022-08-19 10:40:00 +0300

    feat: create 'Main' component

---
+++ client/components/Main/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from '../../styles/Home.module.css'
+
+const Main: NextPage = () => {
+  return (
+    <main className={styles.main}>
+        Main
+      </main>
+  )
+}
+
+export default Main
